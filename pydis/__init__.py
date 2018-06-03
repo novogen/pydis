ab2c76f0750a120356f9b1f1a78b01beb6882ea7
Kyle <kyle@novogen.org>
2018-06-03 15:54:35 -0700

    Added init.py and fixed whitespace

---
+++ pydis/__init__.py
@@ -0,0 +1,4 @@
+from .types import (MachineMode, AddressWidth, DecoderMode, Status, RegisterClass, Feature, CpuFlag, CpuFlagAction,
+                    ExceptionClass, VectorLength, MaskModes)
+
+from .decoder import decode
