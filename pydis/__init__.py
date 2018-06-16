6056a115d450fdab79edb59d1c4f3b3c405c0d5c
Kyle <kyle@novogen.org>
2018-06-16 11:08:58 -0700

    Imported all important types in __init__

--- pydis/__init__.py
+++ pydis/__init__.py
@@ -1,4 +1,18 @@
-from .types import *
+from .types import (AddressFormat, AddressWidth, BroadcastModes, ConversionMode, CpuFlag, CpuFlagAction, DecoderMode,
+                    DecoratorType, DisplacementFormat, ElementTypes, ExceptionClass, Feature, FormatterHookType,
+                    FormatterProperty, FormatterStyle, ImmediateFormat, InstructionAttribute, InstructionEncoding,
+                    LetterCase, MachineMode, MaskModes, MemOpType, OpcodeMap, OperandAction, OperandEncoding,
+                    OperandType, OperandVisibility, RegisterClass, RoundingModes, Status, SwizzleModes, VectorLength)
+from .generate_types import ISAExt, ISASet, InstructionCategory, Mnemonic
 from .zydis_types import MaxInstructionLength, MaxOperandCount, MaxCPUFlag, MaxDecoderMode
 from .formatter import Formatter, default_formatter
 from .decoder import decode
+
+__all__ = ['AddressFormat', 'AddressWidth', 'BroadcastModes', 'ConversionMode', 'CpuFlag', 'CpuFlagAction',
+           'DecoderMode', 'DecoratorType', 'DisplacementFormat', 'ElementTypes', 'ExceptionClass', 'Feature',
+           'Formatter', 'FormatterHookType', 'FormatterProperty', 'FormatterStyle', 'ImmediateFormat',
+           'InstructionAttribute', 'InstructionEncoding', 'LetterCase', 'MachineMode', 'MaskModes', 'MaxCPUFlag',
+           'MaxDecoderMode', 'MaxInstructionLength', 'MaxOperandCount', 'MemOpType', 'OpcodeMap', 'OperandAction',
+           'OperandEncoding', 'OperandType', 'OperandVisibility', 'RegisterClass', 'RoundingModes', 'Status',
+           'SwizzleModes', 'VectorLength', 'decode', 'default_formatter', 'ISAExt', 'ISASet', 'InstructionCategory',
+           'Mnemonic']
